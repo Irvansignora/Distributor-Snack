5f3994cc26b26440c16a92841ddb85186ae1348d
Muhamad Irpan Yasin <143773773+Irvansignora@users.noreply.github.com>
2026-03-11 09:42:53 +0700

    Update ProductDetail.tsx

--- app/src/pages/supplier/ProductDetail.tsx
+++ app/src/pages/supplier/ProductDetail.tsx
@@ -24,9 +24,10 @@ export default function SupplierProductDetail() {
   });
 
   const formatCurrency = (value: number) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('id-ID', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'IDR',
+      minimumFractionDigits: 0,
     }).format(value);
   };
 
