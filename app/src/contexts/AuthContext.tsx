6fa527cda507ac6beaec636826a94a80344bfd82
Muhamad Irpan Yasin <143773773+Irvansignora@users.noreply.github.com>
2026-03-14 13:12:23 +0700

    Update AuthContext.tsx

--- app/src/contexts/AuthContext.tsx
+++ app/src/contexts/AuthContext.tsx
@@ -20,9 +20,6 @@ interface CustomerStore {
   phone_store?: string;
   address_line?: string;
   store_type?: string;
-  bank_name?: string;
-  bank_account_number?: string;
-  bank_account_name?: string;
   allowed_payment_methods?: string[];
   [key: string]: unknown;
 }
